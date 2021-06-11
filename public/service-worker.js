835784ca73b36fe0e0930647599a10daf634b00f
John-Mancini <mancinijohn5988@gmail.com>
2021-06-11 17:18:29 -0400

    added cache and url variables

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE = "site-cache-v1";
+const DATA_CACHE = "data-cache-v1";
+const URL_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/db.js",
+  "/style.css",
+  "/manifest.webmanifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
