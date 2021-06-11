4846ccf20f887658f43e961ccb202e5db38af9d7
John-Mancini <mancinijohn5988@gmail.com>
2021-06-11 17:36:37 -0400

    added fetch listener for api data

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,20 @@ const URL_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(URL_CACHE)));
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.url.includes("/api/")) {
+    e.respondWith(
+      caches.open(DATA_CACHE).then((cache) => {
+        return fetch(e.request).then((response) => {
+          return cache.put(e.request.url, response.clone()).then(() => {
+            return response;
+        });
+      })
+    );
+  }
+});
