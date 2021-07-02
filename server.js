7834c53d2cfcc929b3b93555d12568564b15b633
John-Mancini <mancinijohn5988@gmail.com>
2021-07-02 18:42:29 -0400

    connected mongodb and heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
-
+const MONGO_URL = process.env.MONGO_DB_URL || "mongodb://localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
